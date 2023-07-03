6d7af6855b1185f5b457d62151e17765bffbbcc8
Chakrya Ros <chakryaros@Chakryas-MacBook-Pro-2.local>
2023-07-02 21:22:48 -0500

    Add MTCollatz.java

---
+++ code/MTCollatz.java
@@ -0,0 +1,66 @@
+class MultipleThreadCollatz extends Thread {
+
+    // Class attribute.
+    private int m_num;
+
+    //Create constructor.
+    public MultipleThreadCollatz(int num)
+    {
+        m_num = num;
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            
+            int counter = 0;
+            while( m_num != 1)
+            {
+                if(m_num % 2 != 0)
+                {
+                    m_num = ((3*m_num )+ 1);
+                    counter = counter + 1;
+                    System.out.println("Step: "+ counter + " Collatz Sequence: " + m_num);
+                }else{
+                    m_num = m_num/2;
+                    counter = counter + 1;
+                    System.out.println("Step: "+ counter + " Collatz Sequence: " + m_num);
+                }
+            }
+        }
+        catch (Exception e) {
+            // Throwing an exception
+            System.out.println("Exception is caught");
+        }
+    }
+}
+ 
+// Main Class
+public class MTCollatz {
+    public static void main(String[] args)
+    {
+        
+        int Num = 10000;
+        int T = 8;
+        for (int i = 1; i < T+1; i++) {
+
+            MultipleThreadCollatz object = new MultipleThreadCollatz(Num);
+            object.start();
+           
+            System.out.println("Thread: "+ i);
+            try
+            {
+                System.out.println("Current Thread: "
+                  + Thread.currentThread().getName());
+                 object.join();
+            }
+  
+            catch(Exception ex)
+            {
+                System.out.println("Exception has " +
+                                "been caught" + ex);
+            }
+        }
+    }
+}
